1bdffdc5b7710555b183684824eaf37ece4d84b9
daleonpz <daleonpz@gmail.com>
2024-06-07 19:10:34 +0200

    example t

--- src/app/main.c
+++ src/app/main.c
@@ -1,9 +1,8 @@
 #include <example/example.h>
 
-int main(void)
-{
-	const int expected = 42;
-	int val = ret42();
+int main(void) {
+  const int expected = 42;
+  int val = ret42()
 
-	return (expected != val);
+      return (expected != val);
 }
