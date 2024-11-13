6c6a84ec22141bb88a8de7d364b0f538249336b4
daleonpz <daleonpz@gmail.com>
2024-11-13 18:52:18 +0100

    minor update main.c

--- src/app/main.c
+++ src/app/main.c
@@ -1,9 +1,11 @@
 #include <example/example.h>
+#include <stdio.h>
 
 int main(void)
 {
 	const int expected = 42;
 	int val = ret42();
 
+	printf("ret42() returned %d\n", val);
 	return (expected != val);
 }
